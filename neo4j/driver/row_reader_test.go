49fc304904072e45cee27e99b1a08f0d9ad1155b
Eleanor Deal <eleanor.deal@gmail.com>
2019-03-18 13:20:02 +0000

    Update tests to reduce redundant code by creating close func variable

--- neo4j/driver/row_reader_test.go
+++ neo4j/driver/row_reader_test.go
@@ -10,6 +10,10 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+var closeNoErr = func() error {
+	return nil
+}
+
 func TestBoltRowReader_Read(t *testing.T) {
 
 	Convey("Given a row reader with a mock Bolt reader", t, func() {
@@ -17,9 +21,7 @@ func TestBoltRowReader_Read(t *testing.T) {
 		expectedCSVRow := "the,csv,row"
 
 		mockBoltRows := &internal.BoltRowsMock{
-			CloseFunc: func() error {
-				return nil
-			},
+			CloseFunc: closeNoErr,
 			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
 				return []interface{}{expectedCSVRow, "1,2,3"}, nil, nil
 			},
@@ -50,9 +52,7 @@ func TestBoltRowReader_ReadError(t *testing.T) {
 	Convey("Given a row reader with a mock Bolt reader that returns io.EOF", t, func() {
 
 		mockBoltRows := &internal.BoltRowsMock{
-			CloseFunc: func() error {
-				return nil
-			},
+			CloseFunc: closeNoErr,
 			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
 				return nil, nil, io.EOF
 			},
@@ -84,9 +84,7 @@ func TestBoltRowReader_Read_NoDataError(t *testing.T) {
 	Convey("Given a row reader with a mock Bolt reader that returns a row with no data.", t, func() {
 
 		mockBoltRows := &internal.BoltRowsMock{
-			CloseFunc: func() error {
-				return nil
-			},
+			CloseFunc: closeNoErr,
 			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
 				return []interface{}{}, nil, nil
 			},
@@ -118,9 +116,7 @@ func TestBoltRowReader_Read_TypeError(t *testing.T) {
 	Convey("Given a row reader with a mock Bolt reader that returns a row with no data.", t, func() {
 
 		mockBoltRows := &internal.BoltRowsMock{
-			CloseFunc: func() error {
-				return nil
-			},
+			CloseFunc: closeNoErr,
 			NextNeoFunc: func() ([]interface{}, map[string]interface{}, error) {
 				return []interface{}{666, 666}, nil, nil
 			},
