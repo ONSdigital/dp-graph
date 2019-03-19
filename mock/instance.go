4c682505ceabd2e1187bea9417e774459bd416d2
Eleanor Deal <eleanor.deal@gmail.com>
2019-03-19 15:17:25 +0000

    Add mocked CountInsertedObservations function

--- mock/instance.go
+++ mock/instance.go
@@ -4,6 +4,10 @@ import (
 	"context"
 )
 
+func (m *Mock) CountInsertedObservations(ctx context.Context, instanceID string) (count int64, err error) {
+	return 0, m.checkForErrors()
+}
+
 func (m *Mock) AddVersionDetailsToInstance(ctx context.Context, instanceID string, datasetID string, edition string, version int) error {
 	return m.checkForErrors()
 }
