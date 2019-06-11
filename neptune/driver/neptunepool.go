06ec6df55f1db0f384224653c77cd1e1ba8ae030
peterhoward42 <peterhoward42@gmail.com>
2019-06-11 16:37:31 +0100

    Provide a go-generated mock for NeptunePool

--- neptune/driver/neptunepool.go
+++ neptune/driver/neptunepool.go
@@ -4,23 +4,24 @@ import (
 	gremgo "github.com/gedge/gremgo-neptune"
 )
 
+//go:generate moq -out ./internal/pool.go -pkg internal . NeptunePool
+
 /*
-Interface NeptunePool defines the contract required of the gremgo 
+NeptunePool defines the contract required of the gremgo
 connection Pool by the Neptune.Driver.
 */
 type NeptunePool interface {
+	Close()
 
-    Close()
+	Execute(query string, bindings, rebindings map[string]string) (
+		resp []gremgo.Response, err error)
 
-    Execute(query string, bindings, rebindings map[string]string) (
-        resp []gremgo.Response, err error)
+	Get(query string, bindings, rebindings map[string]string) (
+		resp interface{}, err error)
 
-    Get(query string, bindings, rebindings map[string]string) (
-        resp interface{}, err error)
+	GetCount(q string, bindings, rebindings map[string]string) (
+		i int64, err error)
 
-    GetCount(q string, bindings, rebindings map[string]string) (
-        i int64, err error)
-
-    GetE(q string, bindings, rebindings map[string]string) (
-        resp interface{}, err error)
+	GetE(q string, bindings, rebindings map[string]string) (
+		resp interface{}, err error)
 }
