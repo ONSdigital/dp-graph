74a47a5dd718346e50a48a1a58d0a83c525c14d7
peterhoward42 <peterhoward42@gmail.com>
2019-06-11 17:58:42 +0100

    Provide the first small unit test for neptune/codelists

--- neptune/codelists_test.go
+++ neptune/codelists_test.go
@@ -1,6 +1,7 @@
 package neptune
 
 import (
+	"context"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -9,12 +10,39 @@ import (
 	"github.com/ONSdigital/dp-graph/neptune/internal"
 )
 
-func TestMockDBCompiles(t *testing.T) {
-	Convey("Something", t, func() {
-		poolMock := &internal.NeptunePoolMock{}
-		driver := driver.NeptuneDriver{poolMock}
-		db := &NeptuneDB{driver, 5, 30}
-		_ = db
+// returnOne is a mock implementation for a NeptunePool method.
+var returnOne = func(q string, bindings, rebindings map[string]string) (
+	i int64, err error) {
+	return 1, nil
+}
+
+func TestGetCodeListInNonErrorScenario(t *testing.T) {
+	Convey("When the database says the CodeList ID exists", t, func() {
+		poolMock := &internal.NeptunePoolMock{GetCountFunc: returnOne}
+		driver := driver.NeptuneDriver{Pool: poolMock}
+		db := &NeptuneDB{
+			NeptuneDriver: driver,
+			maxAttempts:   5,
+			timeout:       30,
+		}
+		Convey("And GetCodeList() is called", func() {
+			codeListID := "arbitrary"
+			codeList, err := db.GetCodeList(context.Background(), codeListID)
+			Convey("The driver GetCount function should be called once", func() {
+				calls := poolMock.GetCountCalls()
+				So(len(calls), ShouldEqual, 1)
+				Convey("With a well formed query string", func() {
+					expectedQry := "g.V().hasLabel('_code_list_arbitrary').count()"
+					So(calls[0].Q, ShouldEqual, expectedQry)
+				})
+			})
+			Convey("And no error should be returned", func() {
+				So(err, ShouldBeNil)
+			})
+			Convey("And a non nil structure returned", func() {
+				So(codeList, ShouldNotBeNil)
+			})
+		})
 	},
 	)
 }
