e12096ecd253ee0f79f17e1b15f3300ff7d58caf
Eleanor Deal <eleanor.deal@gmail.com>
2019-09-27 09:40:52 +0100

    Remove use of fold().coalesce(unfold() from dimension creation

--- neptune/query/query.go
+++ neptune/query/query.go
@@ -94,8 +94,10 @@ const (
 	AddInstanceDimensionsPropertyPart = `.property('dimensions', "%s")`
 
 	// dimension
-	CreateDimensionToInstanceRelationship = `g.V().hasLabel('_%s_%s').has('value', "%s").fold().coalesce(unfold(), ` +
-		`addV('_%s_%s').as('d').property('value',"%s").addE('HAS_DIMENSION').V().hasLabel('_%s_Instance').select('d'))`
+	DropDimensionRelationships            = `g.V().hasLabel('_%s_%s').has('value', "%s").bothE().drop().iterate();`
+	DropDimension                         = `g.V().hasLabel('_%s_%s').has('value', "%s").drop().iterate();`
+	CreateDimensionToInstanceRelationship = `g.addV('_%s_%s').as('d').property('value',"%s").addE('HAS_DIMENSION')` +
+		`.V().hasLabel('_%s_Instance').select('d'))`
 
 	// observation
 	DropObservationRelationships   = `g.V().hasLabel('_%s_observation').has('value', "%s").bothE().drop().iterate();`
